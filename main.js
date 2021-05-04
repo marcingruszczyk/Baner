4b0adaf25a874c46ce5db22b77c8ea3e1d5211c4
Marcin Gruszczyk <marcin.gruszczyk95@gmail.com>
2021-05-04 09:32:11 +0200

    Add keyboard control

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const slideList = [{
     img: "images/img1.jpg",
     text: 'Pierwszy tekst'
-}, {
+},
+{
     img: "images/img2.jpg",
     text: 'Drugi tekst'
-}, {
+},
+{
     img: "images/img3.jpg",
     text: 'Trzeci tekst'
-}]
+}];
 
-
-const image = document.querySelector('img.slider')
-const h1 = document.querySelector('h1.slider')
+const image = document.querySelector('img.slider');
+const h1 = document.querySelector('h1.slider');
 const dots = [...document.querySelectorAll('.dots span')]
-// interfejs
+// Interfejs
 const time = 3000;
 let active = 0;
 
-
-// implementacja
+// Implementacje
 
 const changeDot = () => {
-    const activeDot = dots.findIndex(dot => dot.classList.contains('active'))
-    dots[activeDot].classList.remove('active')
-    dots[active].classList.add('active')
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
 }
 
 const changeSlide = () => {
@@ -35,5 +35,24 @@ const changeSlide = () => {
     h1.textContent = slideList[active].text;
     changeDot()
 }
+let indexInterval = setInterval(changeSlide, time)
 
-setInterval(changeSlide, time)
+// Klawisze
+const keyChangeSlide = (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode == 37 || e.keyCode == 39) {
+        clearInterval(indexInterval)
+        e.keyCode == 37 ? active-- : active++;
+        if (active === slideList.length) {
+            active = 0;
+        } else if (active < 0) {
+            active = slideList.length - 1;
+        }
+        image.src = slideList[active].img;
+        h1.textContent = slideList[active].text;
+        changeDot();
+        indexInterval = setInterval(changeSlide, time)
+    }
+}
+
+window.addEventListener('keydown', keyChangeSlide)
