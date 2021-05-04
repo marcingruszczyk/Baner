35949252e58aa53c3e25df7db02ac9df7524b998
Marcin Gruszczyk <marcin.gruszczyk95@gmail.com>
2021-05-04 09:07:55 +0200

    Add method to change dots

--- main.js
+++ main.js
@@ -12,9 +12,19 @@ const slideList = [{
 
 const image = document.querySelector('img.slider')
 const h1 = document.querySelector('h1.slider')
+const dots = [...document.querySelectorAll('.dots span')]
 // interfejs
 const time = 3000;
 let active = 0;
+
+
+// implementacja
+
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'))
+    dots[activeDot].classList.remove('active')
+    dots[active].classList.add('active')
+}
 
 const changeSlide = () => {
     active++;
@@ -23,8 +33,7 @@ const changeSlide = () => {
     }
     image.src = slideList[active].img;
     h1.textContent = slideList[active].text;
+    changeDot()
 }
 
 setInterval(changeSlide, time)
-
-// implementacja
